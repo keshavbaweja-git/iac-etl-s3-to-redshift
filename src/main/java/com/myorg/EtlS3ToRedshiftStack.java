5c76e8f9e70030e3c9a0204ebe8654639fea42f7
Keshav <keshavkb@amazon.com>
2021-12-20 17:08:14 +0800

    Created S3 Managed Policy

--- src/main/java/com/myorg/EtlS3ToRedshiftStack.java
+++ src/main/java/com/myorg/EtlS3ToRedshiftStack.java
@@ -1,10 +1,16 @@
 package com.myorg;
 
 import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.PolicyDocument;
+import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.s3.Bucket;
+
+import static java.util.Arrays.asList;
+import static software.amazon.awscdk.services.iam.Effect.ALLOW;
 
 public class EtlS3ToRedshiftStack extends Stack {
     public EtlS3ToRedshiftStack(final Construct scope, final String id) {
@@ -34,6 +40,29 @@ public class EtlS3ToRedshiftStack extends Stack {
                 .value(processedBucket.getBucketName())
                 .build();
 
+        final PolicyDocument s3ManagedPolicyDocumentAb302 = PolicyDocument.Builder
+                .create()
+                .statements(asList(PolicyStatement
+                                .Builder
+                                .create()
+                                .effect(ALLOW)
+                                .actions(asList("s3:GetObject",
+                                        "s3:PutObject"))
+                                .resources(asList("arn:aws:s3:::" + rawBucket.getBucketName() + "*",
+                                        "arn:aws:s3:::" + processedBucket.getBucketName() + "*"))
+                                .build()))
+                .build();
 
+        final ManagedPolicy s3ManagedPolicyAb302 = ManagedPolicy.Builder
+                .create(this, "s3ManagedPolicyAb302")
+                .document(s3ManagedPolicyDocumentAb302)
+                .managedPolicyName("S3ManagedPolicyAb302")
+                .build();
+
+        CfnOutput.Builder
+                .create(this, "s3ManagedPolicyAb302Name")
+                .exportName("S3ManagedPolicyAb302Name")
+                .value(s3ManagedPolicyAb302.getManagedPolicyName())
+                .build();
     }
 }
