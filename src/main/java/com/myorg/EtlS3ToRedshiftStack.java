5fea02a416a82b77b7c79dfcb0adf668dd7721cb
Keshav <keshavkb@amazon.com>
2021-12-20 18:14:20 +0800

    Attached S3 Managed Policy

--- src/main/java/com/myorg/EtlS3ToRedshiftStack.java
+++ src/main/java/com/myorg/EtlS3ToRedshiftStack.java
@@ -1,16 +1,18 @@
 package com.myorg;
 
 import software.amazon.awscdk.CfnOutput;
-import software.amazon.awscdk.services.iam.ManagedPolicy;
-import software.amazon.awscdk.services.iam.PolicyDocument;
-import software.amazon.awscdk.services.iam.PolicyStatement;
+import software.amazon.awscdk.services.iam.*;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.s3.Bucket;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.util.Arrays.asList;
 import static software.amazon.awscdk.services.iam.Effect.ALLOW;
+import static software.amazon.awscdk.services.iam.ManagedPolicy.*;
 
 public class EtlS3ToRedshiftStack extends Stack {
     public EtlS3ToRedshiftStack(final Construct scope, final String id) {
@@ -43,18 +45,19 @@ public class EtlS3ToRedshiftStack extends Stack {
         final PolicyDocument s3ManagedPolicyDocumentAb302 = PolicyDocument.Builder
                 .create()
                 .statements(asList(PolicyStatement
-                                .Builder
-                                .create()
-                                .effect(ALLOW)
-                                .actions(asList("s3:GetObject",
-                                        "s3:PutObject"))
-                                .resources(asList("arn:aws:s3:::" + rawBucket.getBucketName() + "*",
-                                        "arn:aws:s3:::" + processedBucket.getBucketName() + "*"))
-                                .build()))
+                                           .Builder
+                                           .create()
+                                           .effect(ALLOW)
+                                           .actions(asList("s3:GetObject",
+                                                           "s3:PutObject"))
+                                           .resources(asList("arn:aws:s3:::" + rawBucket.getBucketName() + "*",
+                                                             "arn:aws:s3:::" + processedBucket.getBucketName() + "*"))
+                                           .build()))
                 .build();
 
         final ManagedPolicy s3ManagedPolicyAb302 = ManagedPolicy.Builder
                 .create(this, "s3ManagedPolicyAb302")
+                .path("/service-role/")
                 .document(s3ManagedPolicyDocumentAb302)
                 .managedPolicyName("S3ManagedPolicyAb302")
                 .build();
@@ -62,7 +65,49 @@ public class EtlS3ToRedshiftStack extends Stack {
         CfnOutput.Builder
                 .create(this, "s3ManagedPolicyAb302Name")
                 .exportName("S3ManagedPolicyAb302Name")
-                .value(s3ManagedPolicyAb302.getManagedPolicyName())
+                .value(s3ManagedPolicyAb302.getManagedPolicyArn())
                 .build();
+
+        final String LAKE_FORMATION_WORKFLOW_ROLE_NAME = "LakeFormationWorkflowRoleAb302";
+        final PolicyDocument lakeFormationWorkflowPolicyDocument = PolicyDocument.Builder
+                .create()
+                .statements(asList(PolicyStatement
+                                           .Builder
+                                           .create()
+                                           .effect(ALLOW)
+                                           .actions(asList("lakeformation:GetDataAccess",
+                                                           "lakeformation:GrantPermissions"))
+                                           .resources(asList("*"))
+                                           .build(),
+                                   PolicyStatement
+                                           .Builder
+                                           .create()
+                                           .effect(ALLOW)
+                                           .actions(asList("iam:PassRole"))
+                                           .resources(asList("arn:aws:iam::"
+                                                                     + this.getAccount()
+                                                                     + ":role/" + LAKE_FORMATION_WORKFLOW_ROLE_NAME))
+                                           .build()))
+                .build();
+
+        final Map<String, PolicyDocument> lakeFormationWorkflowPolicyDocuments = new HashMap<>();
+        lakeFormationWorkflowPolicyDocuments.put("LakeFormationWorkflow",
+                                                 lakeFormationWorkflowPolicyDocument);
+        final Role lakeFormationWorkflowRole = Role.Builder
+                .create(this, "lakeFormationWorkflowRole")
+                .roleName(LAKE_FORMATION_WORKFLOW_ROLE_NAME)
+                .assumedBy(new ServicePrincipal("glue.amazonaws.com"))
+                .managedPolicies(asList(fromAwsManagedPolicyName("service-role/AWSGlueServiceRole")))
+                .inlinePolicies(lakeFormationWorkflowPolicyDocuments)
+                .build();
+
+        s3ManagedPolicyAb302.attachToRole(lakeFormationWorkflowRole);
+
+        CfnOutput.Builder
+                .create(this, "lakeFormationWorkflowRoleArn")
+                .exportName(LAKE_FORMATION_WORKFLOW_ROLE_NAME + "Arn")
+                .value(lakeFormationWorkflowRole.getRoleArn())
+                .build();
+
     }
 }
